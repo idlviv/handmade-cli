9742a4311ec8ac055963f6a7731cc068a52ae1d3
idlviv <idlviv@rambler.ru>
2017-03-05 22:43:52 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { routes } from './app.router'
 
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
+import { VizComponent }  from './components/viz.component';
+import { TabComponent }  from './components/tab.component';
+import { TabsComponent }  from './components/tabs.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, routes ],
+  declarations: [ AppComponent, VizComponent, TabsComponent, TabComponent,  AboutComponent,  HomeComponent],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
